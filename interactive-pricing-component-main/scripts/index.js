76d711a64dac09347260fc4ee38cba16c469ab72
Natselane <fabien1308@gmail.com>
2021-02-27 23:54:42 +0100

    Pricing component - add js functionality for updating money amount

--- interactive-pricing-component-main/scripts/index.js
+++ interactive-pricing-component-main/scripts/index.js
@@ -4,20 +4,25 @@ const DOM = (function() {
     const range = document.getElementById("views");
 
     const viewRange = {
-        "1": "10K",
-        "2": "50K", 
-        "3": "100K",
-        "4": "500K",
-        "5": "1M"
+        "1": ["10K", "$8"],
+        "2": ["50K", "$12"], 
+        "3": ["100K", "$16"],
+        "4": ["500K", "$24"],
+        "5": ["1M", "$36"]
     }
 
     const updatePageViews = function(range) {
-        viewsAmount.innerText = `${viewRange[parseInt(range)]}`;
+        viewsAmount.innerText = `${viewRange[parseInt(range)][0]}`;
+    }
+
+    const updateMoneyAmount = function(range) {
+        moneyAmount.innerText = `${viewRange[parseInt(range)][1]}`;
     }
 
     return {
         range,
-        updatePageViews
+        updatePageViews,
+        updateMoneyAmount
     }
 })();
 
@@ -25,4 +30,5 @@ const DOM = (function() {
 
 DOM.range.onchange = function() {
     DOM.updatePageViews(DOM.range.value);
+    DOM.updateMoneyAmount(DOM.range.value);
 }
